cb2d432527f5ca3b40483f80f092c941a9dcd90a
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-05-07 17:12:52 -0400

    operadores comparacao numeros

---
+++ operadores/operador-ternario-04.js
@@ -0,0 +1,4 @@
+const firstCheck = false;
+const secondCheck = false;
+const access = firstCheck ? "Acesso negado" : secondCheck ? "Acesso negado" : "Acesso concedido";
+console. log(access); // Saída: "Acesso concedido"
