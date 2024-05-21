e8cdf67f0c448098c434e59b9ca274f4a629806f
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-05-21 14:39:44 -0400

    funcao/funcao-com-parametro-saudacao.js

---
+++ funcao/funcao-com-parametro-saudacao.js
@@ -0,0 +1,4 @@
+function saudacao (nome) {
+console.log('Olá ${nome}!'); // Saída: Olá Wanderson!
+}
+saudacao("Wanderson"); // Chamada da função passando o parâmetro
---
+++ funcao/funcao-declarativa-soma.js
@@ -0,0 +1,5 @@
+function soma(a, b) {
+return a + b; // Retorna a soma dos parâmetros
+}
+// Chamada da função passando os parâmetros
+console.log(soma(5, 7)); // Saida : 12
